2922e29c12bc2ab83cd008212abc1269c3af6530
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-08-25 20:53:18 +0200

    Add GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,9 @@
 import "../styles/index.scss";
+import Router from "next/router";
+import withGA from "next-ga";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default withGA("UA-133354403-1", Router)(MyApp);
