a7bafb123b60d14dd730fe7d54adefcc97f35f65
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-08-25 20:46:54 +0200

    Update About page

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@ const withPlugins = require("next-compose-plugins");
 const withMdxEnhanced = require("next-mdx-enhanced");
 const mdxPrism = require("mdx-prism");
 const readingTime = require("reading-time");
+const emoji = require("remark-emoji");
+const { rehypeAccessibleEmojis } = require("rehype-accessible-emojis");
 
 const mdx = withMdxEnhanced({
   defaultLayout: true,
   fileExtensions: ["mdx", "md"],
-  rehypePlugins: [mdxPrism],
+  remarkPlugins: [emoji],
+  rehypePlugins: [mdxPrism, rehypeAccessibleEmojis],
   extendFrontMatter: {
     process: (mdxContent) => ({
       readingTime: readingTime(mdxContent),
