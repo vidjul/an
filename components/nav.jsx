ceb9b87e9390aa07e0ba8aadf59b11ad6067682d
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-09-06 14:03:46 +0200

    Enhance navbar

--- components/nav.jsx
+++ components/nav.jsx
@@ -33,38 +33,40 @@ export default class nav extends Component {
     }
 
     return (
-      <nav className="flex flex-wrap items-center justify-between p-4">
-        <div className="w-auto">
-          <Link href="/">
-            <a className={styles.logo}>
-              vidu.sh<span className="highlight">/an</span>
-            </a>
-          </Link>
-        </div>
-        <div className="block lg:hidden">
-          <button
-            onClick={this.handleHamburgerClick}
-            className="flex items-center py-2 px-3 text-teal-500 rounded border border-teal-500"
-          >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+      <div className="sticky top-0 w-full bg-white shadow-xs">
+        <nav className="container mx-auto flex flex-wrap items-center justify-between p-4">
+          <div className="w-auto">
+            <Link href="/">
+              <a className={styles.logo}>
+                vidu.sh<span className="highlight">/an</span>
+              </a>
+            </Link>
+          </div>
+          <div className="block lg:hidden">
+            <button
+              onClick={this.handleHamburgerClick}
+              className="flex items-center py-2 px-3 text-teal-500 rounded border border-teal-500"
             >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-            </svg>
-          </button>
-        </div>
-        <div className={menuClass}>
-          <Link href="/">
-            <a className={styles.navlink}>Home</a>
-          </Link>
-          <Link href="/about">
-            <a className={styles.navlink}>About</a>
-          </Link>
-        </div>
-      </nav>
+              <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <title>Menu</title>
+                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+              </svg>
+            </button>
+          </div>
+          <div className={menuClass}>
+            <Link href="/">
+              <a className={styles.navlink}>Home</a>
+            </Link>
+            <Link href="/about">
+              <a className={styles.navlink}>About</a>
+            </Link>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
