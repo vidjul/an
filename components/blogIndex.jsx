e39873dc9d9cc7c908dcbb2da5e22fa91ef9c01a
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-09-14 21:21:59 +0200

    Enhance blog posts indexes

--- components/blogIndex.jsx
+++ components/blogIndex.jsx
@@ -4,30 +4,54 @@ import { frontMatter as blogPages } from "../pages/posts/*.mdx";
 
 export default function blogIndex() {
   return (
-    <div>
-      <section className="py-12 px-8">
-        <h1 className="py-4">
-          Blog <span className="highlight">entries</span>
-        </h1>
-        {blogPages.map((page) => (
-          <>
-            <div
-              key={page.__resourcePath}
-              className="mx-auto flex flex-wrap justify-between max-w-lg"
-            >
-              <span className="highlight">
-                <Link href={formatPath(page.__resourcePath)}>
-                  <a>{page.title}</a>
-                </Link>
-              </span>
-              <span>{page.date}</span>
-              <span>{page.readingTime.text}</span>
+    <section className="text-gray-700 body-font overflow-hidden">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="-my-8">
+          {blogPages.map((page) => (
+            <div key={page.__resourcePath}>
+              <h3>Blog entries</h3>
+              <div className="py-8 flex flex-wrap md:flex-no-wrap">
+                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                  <span className="tracking-widest font-medium title-font text-gray-900 uppercase">
+                    {page.category}
+                  </span>
+                  <span className="mt-1 text-gray-500 text-sm">
+                    {page.date}
+                  </span>
+                  <span className="mt-1 text-gray-700 text-sm">
+                    {page.readingTime.text}
+                  </span>
+                </div>
+                <div className="md:flex-grow">
+                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
+                    {page.title}
+                  </h2>
+                  <p className="leading-relaxed">{page.description}</p>
+                  <Link href={formatPath(page.__resourcePath)}>
+                    <a className="text-green-500 inline-flex items-center mt-4">
+                      Learn More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+              <hr className="mx-auto" />
             </div>
-            <hr className="mx-auto max-w-md my-2" />
-          </>
-        ))}
-      </section>
-    </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
 
