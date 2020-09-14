88a21898aa963496c959c1b0aa07d78a3318bda3
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-09-14 21:21:59 +0200

    Enhance blog posts indexes

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ import styles from "./footer.module.css";
 export class footer extends Component {
   render() {
     return (
-      <footer className="text-gray-700 body-font">
+      <footer className="text-gray-700 body-font bg-gray-100">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
             © 2020 Vidushan Chooriyakumaran
