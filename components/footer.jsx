d0fd2babeb53185df5629d6a558174bca15d14e3
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-12-23 23:54:23 +0100

    Enhance footer and use fontawesome

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import styles from "./footer.module.css";
 
 export class footer extends Component {
   render() {
     return (
       <footer className="text-gray-700 body-font bg-gray-100">
-        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <div className="container lg:px-32 px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
             © 2020 Vidushan Chooriyakumaran
           </p>
@@ -14,48 +16,16 @@ export class footer extends Component {
               className={styles.social}
               href="https://www.facebook.com/vidushan.chooriyakumaran/"
             >
-              <svg
-                fill="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
+              <FontAwesomeIcon icon={["fab", "facebook"]} size="lg" />
             </a>
             <a className={styles.social} href="https://twitter.com/vidushan_">
-              <svg
-                fill="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
+              <FontAwesomeIcon icon={["fab", "twitter"]} size="lg" />
             </a>
             <a
               className={styles.social}
               href="https://www.linkedin.com/in/c-vidushan/"
             >
-              <svg
-                fill="currentColor"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="0"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="none"
-                  d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-                ></path>
-                <circle cx="4" cy="4" r="2" stroke="none"></circle>
-              </svg>
+              <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" />
             </a>
           </span>
         </div>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,11 @@
 import "../styles/index.scss";
 import Router from "next/router";
 import withGA from "next-ga";
+import { config, library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faCoffee);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
