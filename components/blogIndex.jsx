f2b901f74c9765788c36f9fbf8c60e9eb57a9adf
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2020-12-27 13:10:50 +0100

    Change blog posts to cards

--- components/blogIndex.jsx
+++ components/blogIndex.jsx
@@ -1,52 +1,53 @@
 import React from "react";
 import Link from "next/link";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { frontMatter as blogPages } from "../pages/posts/*.mdx";
 
 export default function blogIndex() {
   return (
-    <section className="text-gray-700 body-font overflow-hidden">
+    <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
-        <div className="-my-8">
-          <h3>Blog entries</h3>
+        <div className="flex flex-wrap -m-4 justify-center">
           {blogPages.map((page) => (
-            <div key={page.__resourcePath}>
-              <div className="py-8 flex flex-wrap md:flex-no-wrap">
-                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="tracking-widest font-medium title-font text-gray-900 uppercase">
-                    {page.category}
-                  </span>
-                  <span className="mt-1 text-gray-500 text-sm">
-                    {page.date}
-                  </span>
-                  <span className="mt-1 text-gray-700 text-sm">
+            <div className="p-4 lg:w-1/3" key={page.__resourcePath}>
+              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                  {page.category}
+                </h2>
+                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                  {page.title}
+                </h1>
+                <p className="leading-relaxed mb-3">{page.description}</p>
+                <Link href={formatPath(page.__resourcePath)}>
+                  <a className="text-indigo-500 inline-flex items-center">
+                    Learn More
+                    <span>
+                      <FontAwesomeIcon
+                        icon="arrow-right"
+                        pull="right"
+                        size="xs"
+                        fixedWidth
+                      />
+                    </span>
+                  </a>
+                </Link>
+                <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                  <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                    <FontAwesomeIcon
+                      icon="hourglass-half"
+                      fixedWidth
+                      pull="left"
+                    />
                     {page.readingTime.text}
                   </span>
-                </div>
-                <div className="md:flex-grow">
-                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                    {page.title}
-                  </h2>
-                  <p className="leading-relaxed">{page.description}</p>
-                  <Link href={formatPath(page.__resourcePath)}>
-                    <a className="text-indigo-500 inline-flex items-center mt-4">
-                      Learn More
-                      <svg
-                        className="w-4 h-4 ml-2"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        fill="none"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M5 12h14"></path>
-                        <path d="M12 5l7 7-7 7"></path>
-                      </svg>
-                    </a>
-                  </Link>
+                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                    <FontAwesomeIcon icon="calendar" fixedWidth pull="left" />
+                    {page.date}
+                  </span>
                 </div>
               </div>
-              <hr className="mx-auto" />
             </div>
           ))}
         </div>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,12 +3,16 @@ import Router from "next/router";
 import withGA from "next-ga";
 import { config, library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRight,
+  faCalendar,
+  faHourglassHalf,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 config.autoAddCss = false;
-library.add(fab, faCoffee);
+library.add(fab, faArrowRight, faHourglassHalf, faCalendar);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
