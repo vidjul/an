a6f68f57e2d18c3b76f774ff16d4a58c23707cf4
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-03-25 20:07:15 +0100

    Config headings and body fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/globals.css";
 
+/** FONTS */
+import "typeface-lora";
+import "typeface-montserrat";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
