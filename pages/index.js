98a24497f9c0ff9ddf3fd2c39c848fd8ef4079ba
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-03-27 19:43:24 +0100

    Add Layout component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useTranslation } from "next-i18next";
 
 import MiniBio from "../components/MiniBio";
 import Header from "../components/Header";
+import Layout from "../components/Layout";
 
 export const getStaticProps = async ({ locale }) => ({
   props: {
@@ -15,8 +16,7 @@ export default function Home() {
   const { t } = useTranslation("common");
 
   return (
-    <>
-      <Header />
+    <Layout>
       <div className="container mx-auto w-3/5 my-16">
         <Head>
           <title>Create Next App</title>
@@ -28,6 +28,6 @@ export default function Home() {
           <MiniBio />
         </main>
       </div>
-    </>
+    </Layout>
   );
 }
