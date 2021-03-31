de5607b28fe726f5f10200a22824c617996df267
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-03-31 20:19:38 +0200

    Change index.js to index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { fetchAPI } from "../lib/api";
+
+import Link from "next/link";
+
+import MiniBio from "../components/MiniBio";
+import Header from "../components/Header";
+import Layout from "../components/Layout";
+
+export const getStaticProps = async ({ locale }) => {
+  const posts = await fetchAPI("/articles");
+
+  return {
+    props: {
+      posts,
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+};
+
+export default function Home({ posts }) {
+  const { t } = useTranslation("common");
+
+  return (
+    <Layout>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="container mx-auto">
+        <div className="flex justify-center items-center h-screen">
+          <div className="prose lg:prose-xl">
+            <h1>{t("introTitle")}</h1>
+            <MiniBio />
+            <Link href="/about">
+              <a>{t("about")}</a>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <ul>
+            {posts.map((post) => {
+              return (
+                <li index={post.id}>
+                  <Link href={`posts/${post.slug}`}>
+                    <a>{post.title}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+}
