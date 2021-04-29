f7a5afafa995ada3beaf29126df195a65a621402
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-04-29 23:32:29 +0200

    Add Logo component

---
+++ components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+  return (
+    <span className="font-logo font-bold text-xl">
+      vidu.sh<span className="text-green-500">/an</span>
+    </span>
+  );
+}
