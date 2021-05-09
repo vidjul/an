8b9b9d10697f600175a38a0604450b94d2a77b25
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-05-09 12:20:05 +0200

    Add eslint

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,21 +2,32 @@ import React from "react";
 
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Logo from "./Logo";
 
 export default function Header() {
   const router = useRouter();
 
   return (
-    <nav className="h-16 px-8 py-4 shadow-lg sticky top-0 w-full bg-white">
-      <span className="font-logo font-semibold text-lg">
-        vidu.sh
+    <div class="container mx-auto border-b-1 p-5 sticky top-0 shadow-sm">
+      <nav class="flex justify-between">
         <Link href="/">
-          <a className="text-green-500">/an</a>
+          <a>
+            <Logo />
+          </a>
         </Link>
-        <Link href="" locale={router.locale === "en" ? "fr" : "en"}>
-          <a className="underline hover:no-underline">/{router.locale}</a>
-        </Link>
-      </span>
-    </nav>
+        <ul class="flex flex-row">
+          <li class="pr-5">
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li className="pr-5">
+            <Link href="" locale={router.locale === "en" ? "fr" : "en"}>
+              <a>{router.locale.toUpperCase()}</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
