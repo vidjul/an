96ef121e80b11d6d15bdee01228e4659015ded8c
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-05-24 14:38:26 +0200

    Fix missing package updates

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
