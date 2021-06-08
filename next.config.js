fd08dd2dbd75a783493284690351a67412fe3025
Vidushan CHOORIYAKUMARAN <vidush@n-c.dev>
2021-06-08 20:16:36 +0200

    Add /an handling!
    Finally!!

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const { i18n } = require("./next-i18next.config");
 
 module.exports = {
   i18n,
+  basePath: "/an",
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
