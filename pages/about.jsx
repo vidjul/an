66500c49271c6c9b90b7281e202d60599c7da9ab
Vidushan Chooriyakumaran <vidush@n-c.dev>
2022-03-12 12:33:17 +0100

    Add back about page

---
+++ pages/about.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+import Layout from "../components/layout";
+
+const layoutProps = {
+  seo: {
+    title: 'Vidushan Chooriyakumaran | Home',
+    description: 'Vidushan Chooriyakumaran\'s personal website.',
+  },
+};
+
+export default function AboutPage() {
+  return (
+    <>
+      <Layout seo={layoutProps.seo}>
+        <main className="prose lg:prose-xl">
+          <h2>Who I am</h2>
+
+          <p>Hello, I am Vidushan! 👋<br /><br/>
+    I am a 26 years old Software Engineer based in Paris. When I'm not coding, I will usually spend my time playing video games, watching anime, or sharing moments with my cousins and family.
+          </p>
+
+          <Link href="/cv.pdf">Résumé</Link>
+
+          <h2>What is this website about</h2>
+
+          <p>This website will mainly host my blog, on which I aim to share posts about tech (tools I use or I have discovered) and my hobbies. Creating and maintaining this website is also a way for me to improve my skills with ReactJS, a library that I love!</p>
+
+          <h2>Contact & Info</h2>
+          
+          <ul>
+            <li><Link href="mailto:vidush@n-c.dev">Mail</Link></li>
+            <li><Link href="https://linkedin.com/in/c-vidushan">LinkedIn</Link></li>
+            <li><Link href="https://github.com/vidjul">Github</Link></li>
+            <li>
+              <Link href="https://myanimelist.net/profile/vidjul">MyAnimeList</Link>
+              {" "}(Just in case you want to share some theories about Attack On Titan)
+            </li>
+          </ul>
+        </main>
+      </Layout>
+    </>
+  );
+}
