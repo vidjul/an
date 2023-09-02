dce8105a0425f42c7c1eb9087f72788221d3c033
Vidushan Chooriyakumaran <vidush@n-c.dev>
2023-09-02 18:44:05 +0200

    chore: rm next mdx

--- next.config.js
+++ next.config.js
@@ -3,11 +3,9 @@ const nextConfig = {
   basePath: "/an",
   experimental: {
     appDir: true,
-    mdxRs: true,
   },
 };
 
-const withMDX = require("@next/mdx")();
 const { withContentlayer } = require("next-contentlayer");
 
-module.exports = withContentlayer(withMDX(nextConfig));
+module.exports = withContentlayer(nextConfig);
