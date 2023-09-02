00e26dd040fc65c6767898c874db1392bfc971d9
Vidushan Chooriyakumaran <vidush@n-c.dev>
2023-09-02 17:16:33 +0200

    fix: make header sticky

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from "./Breadcrumbs";
 
 const Header = () => {
   return (
-    <header className="px-4 py-8 font-display text-xl font-bold md:px-16">
+    <header className="sticky top-0 bg-white px-4 py-8 font-display text-xl font-bold shadow-sm md:px-16">
       <span className="">vidu.sh</span>
       <Breadcrumbs />
     </header>
