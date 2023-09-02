b9cf3737b6b5ec62314d138fa4aba5f40388e7d3
Vidushan Chooriyakumaran <vidush@n-c.dev>
2023-09-02 13:47:31 +0200

    feat: add link to root

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <header className="px-4 py-8 md:px-16">
       <span className="font-display text-xl font-bold">
         vidu.sh
-        <span className="text-blue-500">/an</span>
+        <Link href="/" className="text-blue-500 hover:underline">
+          /an
+        </Link>
       </span>
     </header>
   );
