5f3cecc7c32384b8b823e0da38f8552787931b91
Vidushan Chooriyakumaran <vidush@n-c.dev>
2024-01-06 15:07:48 +0100

    fix: adjust posts list rendering

--- components/PostsList.tsx
+++ components/PostsList.tsx
@@ -1,26 +1,27 @@
+import { PostOrPage } from "@tryghost/content-api";
 import { compareDesc, format, parseISO } from "date-fns";
 import Link from "next/link";
 
-const PostsList = ({ allPosts }) => {
+const PostsList = ({ allPosts }: { allPosts: PostOrPage[] }) => {
   const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.created_at), new Date(b.created_at));
+    return compareDesc(new Date(a.created_at!), new Date(b.created_at!));
   });
 
   return (
     <>
-      {posts.map((page) => (
-        <div key={page.url} className="flex flex-col">
+      {posts.map((post) => (
+        <div key={post.url} className="my-4 flex flex-col">
           <span className="mt-1 text-sm text-neutral-500">
-            {format(parseISO(page.created_at), "dd MMM. — yyyy")}
+            {format(parseISO(post.created_at!), "yyyy — dd MMM.")}
           </span>
           <Link
             className="font-semi-bold mb-1 font-display text-2xl hover:text-sky-600/40"
-            href={`/posts/${page.slug}`}
+            href={`/posts/${post.slug}`}
           >
-            {page.title}
+            {post.title}
           </Link>
 
-          <p className="text-neutral-600">{page.description}</p>
+          <p className="text-neutral-600">{post.excerpt}</p>
         </div>
       ))}
     </>
