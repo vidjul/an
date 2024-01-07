6e326c0521e4b3cb679ab1da15d68cd08ab37108
Vidushan Chooriyakumaran <vidush@n-c.dev>
2024-01-07 11:25:59 +0100

    fix: use rehype to parse and transform raw html to jsx

--- app/(main)/posts/[slug]/page.tsx
+++ app/(main)/posts/[slug]/page.tsx
@@ -1,14 +1,65 @@
 import { format, parseISO } from "date-fns";
 import { getSinglePost } from "lib/posts";
 import Head from "next/head";
+import Image from "next/image";
 import { notFound } from "next/navigation";
+import React from "react";
+import * as prodRuntime from "react/jsx-runtime";
+import rehypeParse from "rehype-parse";
+import rehypeReact, { Options } from "rehype-react";
+import { unified } from "unified";
+
+// @ts-expect-error: Types are missing
+const { Fragment, jsx, jsxs } = prodRuntime;
+
+const options: Options = {
+  Fragment: Fragment,
+  jsx: jsx,
+  jsxs: jsxs,
+  components: {
+    img: (props) => {
+      const { src, alt, height, width } = props;
+
+      if (src) {
+        return (
+          <Image
+            src={src}
+            alt={alt ? alt : ""}
+            height={Number(height)}
+            width={Number(width)}
+          />
+        );
+      }
+    },
+  },
+};
+
+const postProcessor = unified()
+  .use(rehypeParse, { fragment: true })
+  .use(rehypeReact, options);
+
+export async function generateMetadata({ params }) {
+  let title = "...";
+
+  const post = await getSinglePost(params.slug);
+
+  if (post?.title) {
+    title = post.title;
+  }
+
+  return {
+    title,
+  };
+}
 
 const PostPage = async ({ params }) => {
   const post = await getSinglePost(params.slug);
 
-  if (!post) {
+  if (!post?.html) {
     notFound();
   }
+
+  const content = postProcessor.processSync(post.html).result;
 
   return (
     <>
@@ -22,7 +73,7 @@ const PostPage = async ({ params }) => {
             {format(parseISO(post.created_at), "LLLL d, yyyy")}
           </time>
         )}
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        {content}
       </div>
     </>
   );
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   basePath: "/an",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.ghost.org",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
