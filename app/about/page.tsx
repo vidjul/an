95bc32fd934c84dcd98d5b88d9ace5f4a98eb75b
Vidushan Chooriyakumaran <vidush@n-c.dev>
2024-05-08 17:12:08 +0200

    fix: update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,37 +11,38 @@ export default function AboutPage() {
     <main className="prose lg:prose-xl prose-headings:font-display">
       <h1>About</h1>
 
-      <h2>Who I am</h2>
       <Intro />
 
       <Link href="/resume.pdf">Résumé</Link>
 
-      <h2>What is this website about</h2>
-
       <p>
-        This website will mainly host my blog, on which I aim to share posts
-        about tech (tools I use or I have discovered) and my hobbies. Creating
-        and maintaining this website is also a way for me to improve my skills
-        with ReactJS, a library that I love!
+        I have created this website to use it as a way to share knowledge and
+        learnings about software engineering, as well as my hobbies.
       </p>
 
-      <h2>Contact & Info</h2>
-
-      <ul>
-        <li>
-          <Link href="mailto:vidush@n-c.dev">Mail</Link>
-        </li>
-        <li>
-          <Link href="https://linkedin.com/in/c-vidushan">LinkedIn</Link>
-        </li>
-        <li>
-          <Link href="https://github.com/vidjul">Github</Link>
-        </li>
-        <li>
-          <Link href="https://myanimelist.net/profile/vidjul">MyAnimeList</Link>{" "}
-          (Just in case you want to share some theories about Attack On Titan)
-        </li>
-      </ul>
+      <p>
+        If you want to reach out, you can use any of these:
+        <ul>
+          <li>
+            <Link href="mailto:vidush@n-c.dev">Mail</Link>
+          </li>
+          <li>
+            <Link href="https://linkedin.com/in/c-vidushan">LinkedIn</Link>
+          </li>
+          <li>
+            <Link href="https://github.com/vidjul">Github</Link>
+          </li>
+          <li>
+            <Link href="https://myanimelist.net/profile/vidjul">
+              MyAnimeList
+            </Link>{" "}
+            <i>
+              (Please note that this one should be used solely for sharing
+              theories about Attack On Titan)
+            </i>
+          </li>
+        </ul>
+      </p>
     </main>
   );
 }
