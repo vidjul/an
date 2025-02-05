794969440d72740f414c6bc23676e5394e3e167e
Vidushan Chooriyakumaran <vidush@n-c.dev>
2025-02-05 22:46:42 +0100

    feat: add cursor blink when hovering header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,42 @@
-import Link from "next/link";
+"use client";
+
+import { useEffect, useRef, useState } from "react"
+
 import Breadcrumbs from "./Breadcrumbs";
 
 const Header = () => {
+  const [showCursor, setShowCursor] = useState(false)
+  const ref = useRef<HTMLElement>(null);
+
+  const handleOnEnter = () => {
+    setShowCursor(true)
+  }
+
+  const handleOnLeave = () => {
+    setShowCursor(false)
+  }
+
+  useEffect(() => {
+    let headerNode : HTMLElement | null = null
+    if (ref.current) {
+      headerNode = ref.current
+      headerNode.addEventListener("mouseenter", handleOnEnter)
+      headerNode.addEventListener("mouseleave", handleOnLeave)
+
+      return () => {
+        if (headerNode) {
+          headerNode.removeEventListener("mouseenter", handleOnEnter)
+          headerNode.removeEventListener("mouseleave", handleOnLeave)
+        }
+      }
+    }
+  }, [])
+
   return (
-    <header className="sticky top-0 z-10 bg-white px-4 py-8 font-display text-xl shadow-xs md:px-16">
-      <span className="">vidu.sh</span>
+    <header ref={ref} className="sticky top-0 z-10 bg-white px-4 py-8 font-display text-xl shadow-xs md:px-16">
+      <span>vidu.sh</span>
       <Breadcrumbs />
+      {showCursor &&<span className="animate-[blink_1s_step-end_infinite]">_</span>}
     </header>
   );
 };
