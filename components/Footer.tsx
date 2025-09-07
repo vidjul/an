4a178828eef7561e87f2c7523aa41425670e5424
Vidushan Chooriyakumaran <vidush@n-c.dev>
2025-09-07 15:56:17 +0200

    Update tailwind

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <footer className="border-sky-600/400 mx-4 mt-8 border-t-[1px] p-4 text-center text-slate-400 md:mx-16">
+  <footer className="border-sky-600/400 mx-4 mt-8 border-t p-4 text-center text-slate-400 md:mx-16">
     Thanks for passing by!
   </footer>
 );
